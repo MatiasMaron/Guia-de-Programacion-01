5ebd70d11179ade6ad17652e5591c048bda4eb31
MARON MATIAS <46878355@ort>
2022-03-29 10:40:10 -0300

    Ej01 added

--- Assets/Scripts/Ej02.cs
+++ Assets/Scripts/Ej02.cs
@@ -2,16 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ej01 : MonoBehaviour
+public class Ej02 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        int num1 = 0;
-        num1 = num1 + 2;
-        Debug.Log("Su numero es, "+num1);
-        num1 = num1 * num1;
-        Debug.Log("Su numero es, "+num1);
+        
     }
 
     // Update is called once per frame
